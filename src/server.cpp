db9b6b7ea125f7a63835382d47678ebe78f50b82
Julien Maffre <jumaffre@microsoft.com>
2021-10-08 14:47:53 +0000

    Super basic ping pong TPC server

--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,59 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <string>
+
 #include <nghttp2/nghttp2.h>
 
-int main(int argc, char **argv) { return 0; }
+constexpr size_t port = 8080;
+
+int main(int argc, char **argv) {
+
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  std::string hello = "Hello from server";
+
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    return 1;
+  }
+
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                 sizeof(opt))) {
+    return 1;
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(port);
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    return 1;
+  }
+
+  if (listen(server_fd, 3) < 0) {
+    return 1;
+  }
+
+  std::cout << "listening on port " << port << "..." << std::endl;
+
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                           (socklen_t *)&addrlen)) < 0) {
+    perror("accept");
+    exit(EXIT_FAILURE);
+  }
+
+  while (true) {
+    valread = read(new_socket, buffer, 1024);
+    auto msg = std::string(buffer, buffer + valread);
+    send(new_socket, msg.c_str(), msg.size(), 0);
+  }
+
+  return 0;
+}
